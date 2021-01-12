f8651057f0448f0285c172c1facb8b925eedaf67
Danilo Lustosa <danilolustosa@gmail.com>
2021-01-11 21:18:13 -0300

    First commit

---
+++ Aula1_2021/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Aula1_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCompleto = "Dan Lustosa";
+
+            double nota1 = 9;
+            double nota2 = 6;
+            double nota3 = 5;
+            double nota4 = 10;
+
+            double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            Console.WriteLine($"Olá, {nomeCompleto}! A sua média é: {media}");
+
+
+
+
+
+
+            //int numero = 1;
+            //int idade = 17;
+
+            //string nome = "Dan";
+            //string sobrenome = "Lustosa";
+            //string nomeCompleto = nome + " " + sobrenome + ", a minha idade é " + idade + " anos";
+
+            //Console.WriteLine(nomeCompleto);
+
+
+
+
+
+
+            //double valor = 10.50;
+
+            //bool ehVerao = false;
+            //bool ehInverno = false;
+
+
+            //if (nome == "Janna")
+            //{
+            //Console.WriteLine("Olá, Janna! Seja bem vinda!");
+            //}
+            //else
+            //{
+            //Console.WriteLine($"Olá, {nome}!");
+            //}
+
+
+        }
+    }
+}
